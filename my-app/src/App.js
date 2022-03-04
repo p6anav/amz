8c1d29848df275b07711659d8849fcac4bff0e90
p6anav <p6anav2001@gmail.com>
2022-03-04 10:45:41 +0530

    git

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">sprout</a>
+      </header>
+      <div>
+        
+      </div>
+      <main>list of palnts</main>
+    </div>
+  );
+}
+
+export default App;
